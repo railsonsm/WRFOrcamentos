e96896bf54e0fb8e28e6ffa5e36224b05f06b3b4
railson <railson@CPD02.distribuidora.ba>
2017-08-09 16:19:13 -0300

    Adicionado Beans

--- WRFOrcamentos/src/br/cairu/pi/entidade/Cliente.java
+++ WRFOrcamentos/src/br/cairu/pi/entidade/Cliente.java
@@ -1,5 +1,92 @@
 package br.cairu.pi.entidade;
 
-public class Cliente {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private Integer codigo;
+	
+	@Column(name="nome", nullable=false) //Nullable = NOT NULL
+	private String nome;
+	
+	@Column(name="nomeFantasia", nullable=false)
+	private String nomeFantasia;
+	
+	@Column(name="cnpj", nullable=false)
+	private Integer cnpj;
+	
+	@Column(name="rua", nullable=false)
+	private String rua;
+	
+	@Column(name="numero", nullable=false)
+	private Integer numero;
+	
+	@Column(name="complemento", nullable=false)
+	private String complemento;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public Integer getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(Integer cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	
 }
